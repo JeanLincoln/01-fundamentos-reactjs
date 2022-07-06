73b63f4601a028a880c82d13e19f233e211e7aac
Jean Lincoln <jeanlincol@gmail.com>
2022-07-06 03:02:09 -0700

    first commit

---
+++ src/components/Post.jsx
@@ -0,0 +1,51 @@
+import styles from './Post.module.css'
+
+export function Post(){
+    return(
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img 
+                        className={styles.avatar} 
+                        src="https://github.com/JeanLincoln.png" 
+                        alt="Profile-Photo" />
+                        <div className={styles.authorInfo}>
+                            <strong>Jean Lincoln</strong>
+                            <span>Web Developer</span>
+                        </div>
+                </div>
+                <time title="11 de maio ás 08:13" dateTime='2022-05-11 08:13:30'>Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p><a href='#'>jane.design/doctorcare</a></p>
+                <p>
+                    <a href='#'>#novoprojeto</a> {' '}
+                    <a href='#'>#nlw </a> {' '}
+                    <a href='#'>#rocketseat</a> {' '}
+                </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu comentario</strong>
+
+                <textarea placeholder='Deixe seu comentário'></textarea>
+                <footer>
+                <button type='submit'>Publicar</button>
+
+                </footer>
+            </form>
+
+        </article>
+    )
+}
+
+
+
+
+
+
+ 
+
